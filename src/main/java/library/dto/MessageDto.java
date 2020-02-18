6b9a75b00c0aaeade0de9f0fd7874c80597a3578
DedovetsNS <kokocik3@bk.ru>
2020-02-18 11:31:48 +0300

    sending messages in other service now

--- src/main/java/library/dto/MessageDto.java
+++ src/main/java/library/dto/MessageDto.java
@@ -2,11 +2,13 @@ package library.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto implements Serializable {
     private String to;
     private String subject;
